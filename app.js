0e6e823db6cdf525d7c5c998fb97b64cb79c1b1f
KunalGupta <g.kunal9@gmail.com>
2024-03-26 19:09:18 +0530

    vue basics book list

---
+++ app.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+    data () {
+      return {
+        url: 'https://www.amazon.in/',
+        showBooks: true,
+        books: [
+          {
+            title: 'book 1 title',
+            author: 'book 1 author',
+            img: 'assets/1.jpg',
+            isFav: true
+          },
+          {
+            title: 'book 2 title',
+            author: 'book 2 author',
+            img: 'assets/2.jpg',
+            isFav: false
+          },
+          {
+            title: 'book 3 title',
+            author: 'book 3 author',
+            img: 'assets/3.jpg',
+            isFav: false,
+          }
+        ]
+      }
+    },
+    methods: {
+      toggleShowBooks () {
+        this.showBooks = !this.showBooks
+      },
+      toggleIsFav (book) {
+        book.isFav = !book.isFav
+      }
+    },
+    computed:{
+      filteredbooks(){
+        return this.books.filter((book)=>book.isFav)
+      }
+    }
+  })
+  
+  app.mount('#app')
+  
